907691029199c35b30fdb979e026d288fd87340d
rajat12890 <pariharrajat76@gmail.com>
2025-07-08 21:57:06 +0530

    Update extract_image_text.py

--- utils/extract_image_text.py
+++ utils/extract_image_text.py
@@ -1,9 +1,9 @@
-from transformers import pipeline
-from PIL import Image
+# from transformers import pipeline
+# from PIL import Image
 
-# Load once (global or in function)
-ocr_pipeline = pipeline("image-to-text", model="microsoft/trocr-base-printed")
+# # Load once (global or in function)
+# ocr_pipeline = pipeline("image-to-text", model="microsoft/trocr-base-printed")
 
-def extract_text_from_image(image: Image.Image) -> str:
-    result = ocr_pipeline(image)
-    return result[0]["generated_text"] if result else ""
+# def extract_text_from_image(image: Image.Image) -> str:
+#     result = ocr_pipeline(image)
+#     return result[0]["generated_text"] if result else ""
